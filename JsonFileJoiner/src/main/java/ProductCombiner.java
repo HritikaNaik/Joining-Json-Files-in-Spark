ba233963b45906a8bc0639bf6704731e6a8b3bde
Hritika Naik <43138328+HritikaNaik@users.noreply.github.com>
2022-06-29 01:03:51 +0530

    Add files via upload

---
+++ JsonFileJoiner/src/main/java/ProductCombiner.java
@@ -0,0 +1,28 @@
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.Dataset;
+
+public class ProductCombiner {
+    public static void main(String[] args)throws Exception {
+        run(args);
+    }
+    public static void run(String args[])throws Exception {
+        SparkSession sess = SparkSession.builder().appName("json-file-joiner").getOrCreate();
+
+        String outputPath = args[4];;
+
+        Dataset<Row> ds1 = sess.read().json(args[0]);
+        Dataset<Row> ds2 = sess.read().json(args[1]);
+        //Dataset<Row> data3 = sess.read().option("multiline","true").json(args[2]);
+        Dataset<Row> ds4 = sess.read().json(args[3]);
+
+        Dataset<Row> ds2_2 = ds2.withColumnRenamed("product_id", "join_id");
+        Dataset<Row> ds1_2 = ds1.join(ds2_2,ds1.col("product_id").equalTo(ds2_2.col("join_id"))).drop("join_id");
+
+        Dataset<Row> ds4_2 = ds4.withColumnRenamed("product_id", "join_id");
+        Dataset<Row> finalData = ds1_2.join(ds4_2,ds1_2.col("product_id").equalTo(ds4_2.col("join_id"))).drop("join_id");
+
+        finalData.write().mode(SaveMode.Overwrite).json(outputPath);
+    }
+}
