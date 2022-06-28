8ba7ba1268faba42f1027a7f61a16ee20f78496e
Hritika Naik <43138328+HritikaNaik@users.noreply.github.com>
2022-06-29 01:24:47 +0530

    Update ProductCombiner.java

--- JsonFileJoinerSQL/src/main/java/ProductCombiner.java
+++ JsonFileJoinerSQL/src/main/java/ProductCombiner.java
@@ -1,8 +1,6 @@
 
 import org.apache.spark.sql.SaveMode;
-import org.apache.spark.sql.types.*;
 import org.apache.spark.sql.SparkSession;
-//import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.Dataset;
 
@@ -11,25 +9,26 @@ public class ProductCombiner {
         run(args);
     }
 
-    public static void run(String args[]) throws Exception {
-        SparkSession sess = SparkSession.builder().appName("json-file-joiner").getOrCreate();
+    public static void run(String args[]) {
+        SparkSession sess = SparkSession.builder().appName("json-file-joiner").getOrCreate(); //Make the session of spark
 
-        String outputPath = args[4];
-        ;
+        String outputPath = args[4]; //Get the output path from the arguments
 
-        Dataset<Row> ds1 = sess.read().json(args[0]);
-        Dataset<Row> ds2 = sess.read().json(args[1]);
-        //Dataset<Row> data3 = sess.read().option("multiline","true").json(args[2]);
-        Dataset<Row> ds4 = sess.read().json(args[3]);
+        //Read all the inputs from paths given in the arguments
+        Dataset<Row> input1 = sess.read().json(args[0]);
+        Dataset<Row> input2 = sess.read().json(args[1]);
+        Dataset<Row> input3 = sess.read().json(args[2]);
+        Dataset<Row> input4 = sess.read().json(args[3]);
 
-        ds1.createOrReplaceTempView("d1");
-        ds2.createOrReplaceTempView("d2");
-        ds4.createOrReplaceTempView("d4");
+        //Create views from the input datasets that can be used as hive tables in Spark SQL
+        input1.createOrReplaceTempView("data1");
+        input2.createOrReplaceTempView("data2");
+        input3.createOrReplaceTempView("data3");
+        input4.createOrReplaceTempView("data4");
 
-        Dataset<Row> finalData = sess.sql("SELECT * FROM ds1 JOIN ds2 USING (product_id) JOIN ds4 USING (product_id)");
-        //interData.createOrReplaceTempView("iD");
-        //Dataset<Row> finalData = sess.sql("SELECT * FROM iD JOIN d4 USING (product_id)");
+        Dataset<Row> finalData = sess.sql("SELECT * FROM data1 JOIN data2 USING (product_id) JOIN data3 USING (product_id) JOIN data4 USING (product_id)");
 
+        //Overwrite while saving incase there is already an output of a previous job at the given location
         finalData.write().mode(SaveMode.Overwrite).json(outputPath);
     }
 }
